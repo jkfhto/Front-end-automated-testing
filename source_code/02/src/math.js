c2e72c9cec4f4fc1d8c636ebe32dd2fbff2ae053
DESKTOP-C16P34B\jkf <470778561@qq.com>
2019-09-09 09:10:25 +0800

    add   Jest 的简单配置

---
+++ source_code/02/src/math.js
@@ -0,0 +1,17 @@
+export function sum(a, b) {
+    return a + b;
+}
+
+export function minus(a, b) {
+    return a - b;
+}
+
+export function multi(a, b) {
+    return a * b;
+}
+
+// module.exports = {
+//     sum,
+//     minus,
+//     multi
+// }
