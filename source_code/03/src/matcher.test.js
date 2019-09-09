422c7005a181b49c93b4623551c28d1801f73813
DESKTOP-C16P34B\jkf <470778561@qq.com>
2019-09-09 10:26:58 +0800

    update  Jest 匹配器 (Matcher)  演示代码

--- source_code/03/src/matcher.test.js
+++ source_code/03/src/matcher.test.js
@@ -1,19 +1,3 @@
-// import { sum, minus, multi } fr('./math.js');
-import { sum, minus, multi } from "./math.js";
-
-test('测试加法 3+3 等于6', () => {
-    expect(sum(3, 3)).toBe(6);
-})
-
-
-test('测试减法 3-3 等于0', () => {
-    expect(minus(3, 3)).toBe(0);
-})
-
-test('测试乘法 3*3 等于9', () => {
-    expect(multi(3, 3)).toBe(9);
-})
-
 /**
  * Jest 匹配器 (Matcher)
  */
